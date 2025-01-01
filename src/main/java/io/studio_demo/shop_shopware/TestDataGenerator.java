ba8eef12586b662764ffd139f139f92eb7dab33f
RomanSibakovskij <j4023958@gmail.com>
2025-01-01 17:15:34 +0200

    Add empty TestDataGenerator utility sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/TestDataGenerator.java
@@ -0,0 +1,10 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+
+public class TestDataGenerator extends BasePage{
+
+
+    public TestDataGenerator(WebDriver driver) {super(driver);}
+
+}
