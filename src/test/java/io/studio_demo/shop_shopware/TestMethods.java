f02825c6b3f863088045e0c6542ece7024fdb281
RomanSibakovskij <j4023958@gmail.com>
2025-01-01 09:23:54 +0200

    Add basic TestMethods sheet

---
+++ src/test/java/io/studio_demo/shop_shopware/TestMethods.java
@@ -0,0 +1,30 @@
+package io.studio_demo.shop_shopware;
+
+import org.slf4j.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.openqa.selenium.*;
+import java.io.File;
+import java.nio.file.*;
+
+import java.util.*;
+
+public class TestMethods extends BaseTest{
+
+    protected static final Logger logger = LoggerFactory.getLogger(TestMethods.class);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //test result screenshot method
+    public void captureScreenshot(WebDriver driver, String fileName) {
+        try {
+            Thread.sleep(1500);//apply wait time for the right time to get the screenshot (WebDriverWait doesn't work here)
+            File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File destination = new File("D:\\IntelliJ Selenium projects\\DemoWebShopSelenium\\src\\test\\resources\\" + " " +  fileName + ".png");
+            Files.copy(screenshot.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+            logger.info("Screenshot saved at: " + destination.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
