479d0c479362716f32b38731915bca48ca84cb7a
RomanSibakovskij <j4023958@gmail.com>
2025-01-01 15:07:01 +0200

    Add empty LoginRegisterDashboardPage sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/LoginRegisterDashboardPage.java
@@ -0,0 +1,14 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class LoginRegisterDashboardPage extends BasePage{
+
+    public LoginRegisterDashboardPage(WebDriver driver) {super(driver);}
+
+    
+}
