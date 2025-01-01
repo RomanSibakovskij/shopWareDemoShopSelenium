373a3e8d7a0bd1157cfe909f9b40eaa42f9322b1
RomanSibakovskij <j4023958@gmail.com>
2025-01-01 08:07:55 +0200

    Add empty HomePage sheet

---
+++ src/main/java/com/coingatedemoshop/HomePage.java
@@ -0,0 +1,16 @@
+package com.coingatedemoshop;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class HomePage extends BasePage{
+
+
+    public HomePage(WebDriver driver) {super(driver);}
+
+
+}
