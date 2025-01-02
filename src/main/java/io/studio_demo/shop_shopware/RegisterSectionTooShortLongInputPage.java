59bd47474eb1b6d45d45cc94139cbc06546fd46f
RomanSibakovskij <j4023958@gmail.com>
2025-01-02 19:04:56 +0200

    Add empty RegisterSectionTooShortLongInputPage utility sheet to avoid code cluster

---
+++ src/main/java/io/studio_demo/shop_shopware/RegisterSectionTooShortLongInputPage.java
@@ -0,0 +1,11 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class RegisterSectionTooShortLongInputPage extends BasePage{
+
+    public RegisterSectionTooShortLongInputPage(WebDriver driver) {super(driver);}
+
+}
