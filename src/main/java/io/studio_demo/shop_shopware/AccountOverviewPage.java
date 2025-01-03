6511c642b0c17b61265dc3e792e090084ccf169b
RomanSibakovskij <j4023958@gmail.com>
2025-01-03 09:16:06 +0200

    Add empty AccountOverviewPage sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/AccountOverviewPage.java
@@ -0,0 +1,13 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+public class AccountOverviewPage extends BasePage{
+
+
+    public AccountOverviewPage(WebDriver driver) {super(driver);}
+
+
+}
