3617909d2be09c5cb4f5cd61f28a73da1288aab2
RomanSibakovskij <j4023958@gmail.com>
2025-01-03 07:59:46 +0200

    Add relevant web elements for RegisterSectionInvalidSingularInputPage

--- src/main/java/io/studio_demo/shop_shopware/RegisterSectionInvalidSingularInputPage.java
+++ src/main/java/io/studio_demo/shop_shopware/RegisterSectionInvalidSingularInputPage.java
@@ -1,9 +1,27 @@
 package io.studio_demo.shop_shopware;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class RegisterSectionInvalidSingularInputPage extends BasePage{
-    
+
+    //register section web elements
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='personalFirstName']")
+    private WebElement registerSectionFirstNameInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='personalLastName']")
+    private WebElement registerSectionLastNameInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='personalMail']")
+    private WebElement registerSectionEmailInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='personalPassword']")
+    private WebElement registerSectionPasswordInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='billingAddressAddressStreet']")
+    private WebElement registerSectionAddressInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='billingAddressAddressCity']")
+    private WebElement registerSectionCityInputField;
+    @FindBy(xpath = "//div[@class='card register-card']//input[@id='billingAddressAddressZipcode']")
+    private WebElement registerSectionPostalCodeInputField;
+
     public RegisterSectionInvalidSingularInputPage(WebDriver driver) {super(driver);}
 
 
