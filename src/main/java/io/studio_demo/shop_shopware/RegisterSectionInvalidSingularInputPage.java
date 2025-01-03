6b892b9c04b481dab6eaa7cf02e2116ce5bf6871
RomanSibakovskij <j4023958@gmail.com>
2025-01-03 07:58:40 +0200

    Add empty RegisterSectionInvalidSingularInputPage utility sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/RegisterSectionInvalidSingularInputPage.java
@@ -0,0 +1,10 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.WebDriver;
+
+public class RegisterSectionInvalidSingularInputPage extends BasePage{
+    
+    public RegisterSectionInvalidSingularInputPage(WebDriver driver) {super(driver);}
+
+
+}
