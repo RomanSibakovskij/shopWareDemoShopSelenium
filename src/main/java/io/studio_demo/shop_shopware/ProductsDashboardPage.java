7fba572d25995487edeb350ca28dd5ed4609306d
RomanSibakovskij <j4023958@gmail.com>
2025-01-03 18:26:54 +0200

    Rename FreeTimeElectronicsProductsDashboardPage to ProductsDashboardPage

---
+++ src/main/java/io/studio_demo/shop_shopware/ProductsDashboardPage.java
@@ -0,0 +1,13 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+
+public class ProductsDashboardPage extends BasePage{
+
+    //free time & electronics product dashboard page
+
+
+    public ProductsDashboardPage(WebDriver driver) {super(driver);}
+
+
+}
