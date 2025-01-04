7251a922203152cd94be3c7addb78b02a4a03c21
RomanSibakovskij <j4023958@gmail.com>
2025-01-04 18:48:02 +0200

    Add empty CheckoutGuestTooShortLongInputPage utility sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/CheckoutGuestTooShortLongInputPage.java
@@ -0,0 +1,15 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutGuestTooShortLongInputPage extends BasePage{
+
+
+    public CheckoutGuestTooShortLongInputPage(WebDriver driver) {super(driver);}
+
+
+}
