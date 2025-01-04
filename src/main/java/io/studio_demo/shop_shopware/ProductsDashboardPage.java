6df86525b738d21bdbc59475837407fd9cca405e
RomanSibakovskij <j4023958@gmail.com>
2025-01-04 08:19:10 +0200

    Add empty ShoppingCartPage sheet

--- src/main/java/io/studio_demo/shop_shopware/ProductsDashboardPage.java
+++ src/main/java/io/studio_demo/shop_shopware/ProductsDashboardPage.java
@@ -2,8 +2,7 @@ package io.studio_demo.shop_shopware;
 
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.*;
 
 import java.time.Duration;
 import java.util.*;
---
+++ src/main/java/io/studio_demo/shop_shopware/ShoppingCartPage.java
@@ -0,0 +1,16 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class ShoppingCartPage extends BasePage{
+
+
+    public ShoppingCartPage(WebDriver driver) {super(driver);}
+
+    
+}
