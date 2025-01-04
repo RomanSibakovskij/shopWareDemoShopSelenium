65c2109060572aa8be598fc0a4437ceb2459a6c3
RomanSibakovskij <j4023958@gmail.com>
2025-01-04 17:52:14 +0200

    Add empty CheckoutGuestNoSingularInputPage utility page

---
+++ src/main/java/io/studio_demo/shop_shopware/CheckoutGuestNoSingularInputPage.java
@@ -0,0 +1,15 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutGuestNoSingularInputPage extends BasePage{
+
+
+    public CheckoutGuestNoSingularInputPage(WebDriver driver) {super(driver);}
+
+
+}
