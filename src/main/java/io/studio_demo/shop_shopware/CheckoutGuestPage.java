72cc218efe8df8e58a495d9a6976e7c0520c4b5e
RomanSibakovskij <j4023958@gmail.com>
2025-01-04 12:20:08 +0200

    Add empty CheckoutGuestPage sheet (reg user and guest have different checkout pages)

---
+++ src/main/java/io/studio_demo/shop_shopware/CheckoutGuestPage.java
@@ -0,0 +1,15 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class CheckoutGuestPage extends BasePage{
+
+    public CheckoutGuestPage(WebDriver driver) {super(driver);}
+
+
+}
