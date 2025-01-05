f52342f8ec5e89e229cd621870ec89b0f9b8e142
RomanSibakovskij <j4023958@gmail.com>
2025-01-05 16:48:18 +0200

    Add empty ThankYouPage sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/ThankYouPage.java
@@ -0,0 +1,15 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+import java.util.*;
+
+public class ThankYouPage extends BasePage{
+
+    public ThankYouPage(WebDriver driver) {super(driver);}
+
+
+}
