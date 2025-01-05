7f50a3aae002e4031728490fbd92998837ee32ed
RomanSibakovskij <j4023958@gmail.com>
2025-01-05 08:26:57 +0200

    Add empty CheckoutConfirmationPage sheet

---
+++ src/main/java/io/studio_demo/shop_shopware/CheckoutConfirmationPage.java
@@ -0,0 +1,15 @@
+package io.studio_demo.shop_shopware;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.*;
+
+import java.time.Duration;
+
+public class CheckoutConfirmationPage extends BasePage {
+
+
+    public CheckoutConfirmationPage(WebDriver driver) {super(driver);}
+
+
+}
